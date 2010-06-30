3ad0a38a590b641c083367ba13cef1290444ca5a
Dimitris Moraitis <dimo@unweb.me>
2010-06-30 13:41:02 +0000

    add zope.app.twisted to dependencies
    svn path=/collective.transcode.daemon/trunk/; revision=119946

--- setup.py
+++ setup.py
@@ -19,6 +19,7 @@ setup(name='collective.transcode.daemon',
       zip_safe=False,
       install_requires=[
           # -*- Extra requirements: -*-
+          'zope.app.twisted',
       ],
       entry_points="""
       # -*- Entry points: -*-
