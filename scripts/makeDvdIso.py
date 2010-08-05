85a30ea9c8adc41b78fd50bcd1b816be85204528
Markos Gogoulos <mgogoulos@gmail.com>
2010-08-05 00:03:03 +0000

    changes on the make dvd iso python script
    on transcode daemon
    
    
    
    svn path=/collective.transcode.daemon/trunk/; revision=122884

--- scripts/makeDvdIso.py
+++ scripts/makeDvdIso.py
@@ -1,11 +1,12 @@
 #!./bin/daemonpy
 
-import sys, subprocess
+import sys, subprocess, datetime, tempfile, os
 import feedparser
 
 if len(sys.argv) == 3:
     input_rss = sys.argv[1]
     output_iso = sys.argv[2]
+
 else:
     print 'too many or too few arguments in makeDvdIso command'
     sys.exit(1)
@@ -19,26 +20,38 @@ cmd = [u'./bin/todisc', u'-menu-fontsize', u'36', u'-title-color', u'\'#ff7700\'
 
 dvd_title = unicode(rss.feed.title)
 cmd += [u'-menu-title'] 
-cmd += dvd_title
+cmd += [dvd_title]
 
 files = []
 titles = []
+
+tempdir = tempfile.mkdtemp()
+
 for entry in rss.entries:
-    files.append(entry.link)
     titles.append(entry.title)
+
+for video in os.listdir(tempdir):
+    #TODO 1: download file on temp dir 
+    files.append(tempdir+video)
+
 
 cmd += [u'-files'] + files
 cmd += [u'-titles'] + titles
 cmd += [u'-out']
-cmd += unicode(output_iso)
+cmd += [unicode(output_iso)]
 
 try:
     subprocess.call(cmd)
 except:
     print 'problem when calling tovid command'
 
+
+try:
+    subprocess.call("mkisofs -o %s %s" % (output_iso, tempdir))
+except:
+    print 'problem when calling mkisofs command' % 
+
     
-
-
-
+#TODO 2: send callback that dvd is ready
+#TODO 3: delete dvd and folder after 
 
