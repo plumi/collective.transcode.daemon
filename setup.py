e970b51d02f6612b4657f9ca0caaeebc89224541
Dimitris Moraitis <dimo@unweb.me>
2010-10-09 17:37:18 +0000

    Prepare collective.transcode.daemon 0.5.
    svn path=/collective.transcode.daemon/trunk/; revision=145864

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '0.4'
+version = '0.5'
 
 long_description = (
     read('README.txt')
