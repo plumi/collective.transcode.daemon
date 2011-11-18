4f3adb2c90c0fbb9507e497ccfeb29543be92b8d
Christos Psaltis <cpsaltis@unweb.me>
2011-11-18 19:54:57 +0200

    Add mime types

--- collective/transcode/daemon/config.py
+++ collective/transcode/daemon/config.py
@@ -3,26 +3,30 @@ listen_port='8888'
 videofolder='videos'
 secret='1771d99931264d538e75eeb19da7d6a0'
 
-default_supported_mimetypes = [ 'video/mpeg', 
-                                'video/3gpp', 
-                                'video/x-ms-wmv', 
-                                'video/ogg', 
-                                'video/x-ogg', 
-                                'video/x-ogm+ogg', 
-                                'video/quicktime', 
-                                'video/x-la-asf', 
-                                'video/x-ms-asf', 
+default_supported_mimetypes = ['application/ogg',
+                                'video/ogg',
+                                'video/x-ogg',
+                                'video/x-ogm+ogg',
+                                'video/flv',
+                                'video/x-flv',
+                                'video/mpeg',
+                                'video/3gpp',
+                                'video/x-ms-wmv',
+                                'video/quicktime',
+                                'video/x-la-asf',
+                                'video/x-ms-asf',
                                 'video/x-msvideo',
                                 'video/mp4',
-                              ]
+                                'video/webm',
+                                ]
 
 profiles = [
-        {'id' : 'low', 
-         'cmd' : 'ffmpeg -y -i %s -s 424x344 -qscale 5.0 -r 30 -ar 44100 -f flv %s', 
+        {'id': 'low',
+         'cmd': 'ffmpeg -y -i %s -s 424x344 -qscale 5.0 -r 30 -ar 44100 -f flv %s',
          'supported_mime_types': default_supported_mimetypes,
-        }, 
-        {'id' : 'high', 
-         'cmd' : 'ffmpeg -y -i %s -s 640x480 -qscale 5.0 -r 50 -ar 44100 -f flv %s', 
+        },
+        {'id' : 'high',
+         'cmd' : 'ffmpeg -y -i %s -s 640x480 -qscale 5.0 -r 50 -ar 44100 -f flv %s',
          'supported_mime_types': default_supported_mimetypes,
         }
        ]
